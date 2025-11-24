fcd1c94242670921e0c5f65c12ea252a45ab3255
popmonkey <popmonkey@users.noreply.github.com>
2025-11-23 19:13:30 -0800

    Update version and URLs for JresSolver

--- Formula/jres_solver.rb
+++ Formula/jres_solver.rb
@@ -1,17 +1,17 @@
 class JresSolver < Formula
   desc "JSON.Racing Endurance Schedule Solver"
   homepage "https://github.com/popmonkey/jres_solver_cpp"
-  version "9.1.0"
+  version "0.9.2"
 
   if OS.mac?
     if Hardware::CPU.arm?
       # Apple Silicon (M1/M2/M3)
-      url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v1.0.0/jres-solver-1.0.0-macos-arm64.tar.gz"
-      sha256 "5d01168d0a30cd89364496909d790bde028801d4f7281609897922dff2a3cc35"
+      url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v0.9.2/jres-solver-v0_9_2-darwin-arm64.tar.gz"
+      sha256 "923854fa8a7f2822cf1061ef243c0b6facfafa288e3b27e7088b93295c83e494"
     else
       # Intel Mac
-      url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v1.0.0/jres-solver-1.0.0-macos-x86_64.tar.gz"
-      sha256 "28d95d7fe26d7ff4b6009c2a634a5b47cda5904fc6110f210cb35fee49c56989"
+      url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v0.9.2/jres-solver-v0_9_2-darwin-amd64.tar.gz"
+      sha256 "d5fe23ec02069c9d79c6f065fadbd7cc1ba879f3be3ff31ece20818ddeba94e8"
     end
   end
 
@@ -24,7 +24,7 @@ class JresSolver < Formula
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/solver --help", 1)
-    assert_match "Usage", shell_output("#{bin}/formatter --help", 1)
+    assert_match "Usage", shell_output("#{bin}/jres_solver --help", 1)
+    assert_match "Usage", shell_output("#{bin}/jres_formatter --help", 1)
   end
 end
