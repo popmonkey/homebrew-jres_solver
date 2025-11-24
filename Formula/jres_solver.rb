b276205f0f809c5ef9d6e48b847a2a397191a0f3
popmonkey <popmonkey@users.noreply.github.com>
2025-11-23 23:09:37 -0800

    Update SHA256 checksums for jres_solver

--- Formula/jres_solver.rb
+++ Formula/jres_solver.rb
@@ -7,11 +7,11 @@ class JresSolver < Formula
     if Hardware::CPU.arm?
       # Apple Silicon (M1/M2/M3)
       url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v0.9.2/jres-solver-v0_9_2-darwin-arm64.tar.gz"
-      sha256 "923854fa8a7f2822cf1061ef243c0b6facfafa288e3b27e7088b93295c83e494"
+      sha256 "33d88e860898bfc641da81405a4d09e148728c6319b87a6de7cb832218cd3a71"
     else
       # Intel Mac
       url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v0.9.2/jres-solver-v0_9_2-darwin-amd64.tar.gz"
-      sha256 "d5fe23ec02069c9d79c6f065fadbd7cc1ba879f3be3ff31ece20818ddeba94e8"
+      sha256 "4c5e186afde4b685d1292ce53ec75f9ab8ddb4a5b85ffe6cc75b106b8e528951"
     end
   end
 
