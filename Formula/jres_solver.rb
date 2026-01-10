ee7e0d4dfc02da450d9207c9145184b153ae9418
popmonkey <popmonkey@users.noreply.github.com>
2026-01-09 18:21:22 -0800

    Update jres_solver.rb to v3.1.0

--- Formula/jres_solver.rb
+++ Formula/jres_solver.rb
@@ -1,17 +1,17 @@
 class JresSolver < Formula
   desc "JSON.Racing Endurance Schedule Solver"
   homepage "https://github.com/popmonkey/jres_solver_cpp"
-  version "3.0.0"
+  version "3.1.0"
 
   if OS.mac?
     if Hardware::CPU.arm?
       # Apple Silicon (M1/M2/M3)
-      url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v3.0.0/jres-solver-v3_0_0-darwin-arm64.tar.gz"
-      sha256 "68fb491d9a6a4c84df04190d61d2acfa87ab9f9d7b24721a4e3a9078c96166a2"
+      url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v3.1.0/jres-solver-v3_1_0-darwin-arm64.tar.gz"
+      sha256 "2262ff8dc29e09c7c51196412ce018d22479259b5c8e381605103884bfc4646c"
     else
       # Intel Mac
-      url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v3.0.0/jres-solver-v3_0_0-darwin-x64.tar.gz"
-      sha256 "5b451f8b6bc7977e5f12e50c732b0ccd83f6374921bb6703aa93cab46c9c51d8"
+      url "https://github.com/popmonkey/jres_solver_cpp/releases/download/v3.1.0/jres-solver-v3_1_0-darwin-x64.tar.gz"
+      sha256 "d535f0cd4a6b6c76528f974c6e10714fb471dd083ede0e362175e1f6d1d2fc26"
     end
   end
 
